86fbbe90580531ba63e5af9cb3dc63921535566d
Surat <suratmamedov07@gmail.com>
2024-02-11 12:33:48 +0300

    Краски банок

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 120 / (2 + 4); // количество классов
+
+        int whitePaint = n * 2; // количество банок белой краски
+        int brownPaint = n * 4; // количество банок коричневой краски
+
+        System.out.println("В школе, где " + n + " классов, нужно " + whitePaint + " банок белой краски и " + brownPaint + " банок коричневой краски.");
+
+
+
+    }
+}
